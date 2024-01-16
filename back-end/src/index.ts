c37f4b41aa53526fffa6f489f33db7a347024fe9
christopher nafrere <nafrere.christopher@gmail.com>
2024-01-16 08:13:38 +0100

    Initialize project

---
+++ back-end/src/index.ts
@@ -0,0 +1,21 @@
+import express from "express"
+import { Server } from "socket.io"
+
+const app = express()
+const PORT = process.env.PORT || 4000
+
+app.use(express.json())
+
+const server = app.listen(PORT, () => console.log(`Lancement du server sur le port ${PORT}`))
+
+const io = new Server(server)
+
+io.on("connect", (socket) => {
+    console.log("Un utilisateur viens de se connecter")
+
+    socket.on("disconnect", () => {
+        console.log("Un utilisateur viens de se déconnecter")
+    })
+
+    
+})
